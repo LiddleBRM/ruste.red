38661dff3fbb7150e58d827c7302d9c57f0b1f15
LiddleBRM <liddle@ruste.red>
2024-05-14 20:56:11 +0900

    自己紹介のカンマの位置がところどころおかしかったのを修正

--- src/components/about/IntroducctionText/IntroductionText.ts
+++ src/components/about/IntroducctionText/IntroductionText.ts
@@ -1,11 +1,11 @@
 export const IntrodoctionText = [
-  'Julia "Rustered" Scott : Crossdressor with black sclera. It consists of physical body “Julia Scott” and “Liddle Beret Rustered Marshall,” the phantom who controls the body.',
+  'Julia "Rustered" Scott : Crossdressor with black sclera. It consists of physical body “Julia Scott” and “Liddle Beret Rustered Marshall” the phantom who controls the body.',
   "He loves Monstergirls. And he loves the virtual world where many extraordinary looking creatures exist.\n\
-   But he knows that “the virtual world cannot exist without the resources of the real world,” so he decided to remain in the real world.",
+   But he knows that “the virtual world cannot exist without the resources of the real world”, so he decided to remain in the real world.",
 ];
 
 export const CensoredText = [
   "“Liddle Beret Marshall” lost his life on ■■■■■■■■■ ■■, ■■■■. He was attacked by a horde of ruthless monsters called ■■■■■■.",
   'After his death, he meets a breathless man "■■ ■■", who takes over his corpse and leads a “second life”.',
-  '■■ instructed Liddle to call the body “Julia Scott”. Liddle added to that name the middle name “Rustered,” a term he coined for “the man who was once rusted".',
+  '■■ instructed Liddle to call the body “Julia Scott”. Liddle added to that name the middle name “Rustered”, a term he coined for “the man who was once rusted".',
 ];
